8eefc4782854cf3847e6b6a4890db9334bd64494
pashtunzalmay <oolumee@gmail.com>
2020-11-04 19:40:03 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+const api = {
+ key: "878c0213b610ddf6f68989213ccb9a75",
+ base: " http://api.openweathermap.org/data/2.5/"
+}
+
+function getCurrent () {
+  fetch(`${api.base}weather/?q=${Washington, DC}&units=imperial&APPID=${api.key}`)
+  .then(weather => {
+   return weather.json();
+  }).then(displayCurrent);
+}
+
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+ // 13 refers to Enter key on keyboard
+ if(evt.keyCode == 13) {
+  getResults(searchbox.value)
+ }
+}
+
+function getResults(query) {
+ fetch(`${api.base}weather/?q=${query}&units=imperial&APPID=${api.key}`)
+  .then(weather => {
+   return weather.json();
+  }).then(displayResults);
+}
+
+function displayResults(weather) {
+  console.log(weather);
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.main.temp_min)} °F / ${Math.round(weather.main.temp_max)} °F`
+}
+
+function dateBuilder (d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${month} ${date} ${year}`;
+}
